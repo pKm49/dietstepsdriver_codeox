ab9aba7903d275545b609c833a51f5e349dd74c3
Prasanth K M <prasanthkm49@gmail.com>
2024-08-20 20:30:50 +0530

    api backend related changes

--- lib/env.dart
+++ lib/env.dart
@@ -1,7 +1,7 @@
-const apiEndPoint = "vertexlabs.online";
+const apiEndPoint = "68.183.247.240:8069";
 // const apiEndPoint = "139.59.52.48:8069";
 
-const clientId = "6xnfBLxO7cOyZ2tQeU7nQGWtT1zLXq";
-const clientSecret = "AmJVWEDv3Q8Je1ZmPdyiS7ydBJiPNW";
+const clientId = "y9P7ycEWDbPgCoRCetZvkCVBiumFfo";
+const clientSecret = "xcfcTlmDEN2lZtz41r8lJybwWrxhSq";
 const appStorePackageId = "com.codeox.nutriprodriver";
 const playStorePackageId = "com.nutribites.nutripro_driver";
